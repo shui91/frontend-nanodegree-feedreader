7746efe9f6de963a207adbc8422cfb07f7301a57
shui91 <sherman.sy.hui@gmail.com>
2015-07-19 22:24:05 -0700

    feat: separated variables in allFeed mod suite so tests can run individually, assigned new variables for each state of allFeed.length prior and after adding/removing a feed fix: made fixes according to Udacity reviewer, missing semicolons, changed TODO: -> DONE:

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -27,7 +27,7 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* DONE: Wrote a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
@@ -38,7 +38,7 @@ $(function() {
             });
          });
 
-        /* TODO: Write a test that loops through each feed
+        /* DONE: Wrote a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
@@ -51,10 +51,10 @@ $(function() {
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
+    /* DONE: Wrote a new test suite named "The menu" */
 
     describe('The Menu', function(){
-        /* TODO: Write a test that ensures the menu element is
+        /* DONE: Wrote a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
@@ -87,9 +87,9 @@ $(function() {
         });
     });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+    /* DONE: Wrote a new test suite named "Initial Entries" */
     describe('Initial Entries', function() {
-        /* TODO: Write a test that ensures when the loadFeed
+        /* DONE: Wrote a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test wil require
@@ -109,9 +109,9 @@ $(function() {
          });
     });
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
+    /* DONE: Wrote a new test suite named "New Feed Selection" */
     describe('News Feed Selection', function(){
-        /* TODO: Write a test that ensures when a new feed is loaded
+        /* DONE: Wrote a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
@@ -153,30 +153,25 @@ $(function() {
         var newURL = 'www.testurl.com';
         var newFeedItem = {name: newName, url: newURL};
 
-        var FeedLen = allFeeds.length; // set the original feed length prior to adding
-        var newItem; // set new feed item variable here
+        var feedLen = allFeeds.length; // val: 5 - get the original feed length prior to adding/subtracting
 
         it('should add a new feed', function(){
             addFeed(newName, newURL);
-            expect(FeedLen).toBe(FeedLen++); // after addFeed, we should see the feed length increase by 1
+            var newFeedLen = allFeeds.length; // val: 6 after adding a feed
+            expect(newFeedLen).toBe(feedLen + 1); // after addFeed, we should see the feed length increase by 1
             expect(allFeeds[allFeeds.length-1]).toMatch(newFeedItem);
             // removed these tests in favor of the above test for DRY purposes
             // expect(allFeeds[allFeeds.length-1].name).toBe(newName); 
             // expect(allFeeds[allFeeds.length-1].url).toBe(newURL);
-            newItem = allFeeds[allFeeds.length-1]; //update new feed item to be the test subject
-            //console.log(newItem);
-            console.log('len after add test ' + FeedLen);
-            console.log(allFeeds);
         });
 
         it('should remove a feed', function(){
-            var trackRemoved = allFeeds[0]; // the original 0th feed before removeal
+            var trackRemoved = allFeeds[0]; // the original 0th feed before removal
+            var ogFeedLen = allFeeds.length; // feed length before removal
+
             removeFeed(0); // removing 0th feed
-            console.log(allFeeds);
-            console.log('len after remove test ' + allFeeds.length)
-            console.log('stored item FeedLen is ' + FeedLen)
-            //console.log(newFeedLen, "newLen"); //check feed length after removal
-            expect(allFeeds.length).toEqual(FeedLen-1);
+            var rmFeedLen = allFeeds.length; // updated feed length
+            expect(rmFeedLen).toEqual(ogFeedLen - 1);
             expect(allFeeds[0]).not.toBe(trackRemoved); // check if 0th feed has been removed
             //expect(allFeeds[allFeeds.length-1]).not.toMatch[newItem]; // old version where I would check if addFeed's feed was still in the array 
         });
